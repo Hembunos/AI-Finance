9a97d290ba2676c3f768bb5d28099d70be7fde3d
Hembunos <hembunospro1428@gmail.com>
2025-02-24 15:35:18 +0530

    file name changed

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "AI Finance",
+  title: "WealthWise",
   description: "One Stop Finance Platform",
 };
 
